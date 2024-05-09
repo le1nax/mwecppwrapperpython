75c86737f020308c1249395ef286f8b5ca6357c1
daniel.geiger <daniel.geiger@rwth-aachen.de>
2024-05-09 15:18:02 +0200

    pybind11 mwe

--- code/main.cpp
+++ code/main.cpp
@@ -11,6 +11,21 @@ float addFloat(float arg1, float arg2)
     return arg1 + arg2;
 }
 
+class TestClass{
+    public:
+        TestClass(float mul){
+            multiplier = mul;
+        }
+    private:
+
+        float multiplier;
+
+    public:
+        float multiply(float input_mul){
+            return input_mul*multiplier;
+        }
+}
+
 PYBIND11_MODULE(module_name, handle){
     handle.doc() = "This is the module docs.";
     handle.def("addFloat", &addFloat); 
